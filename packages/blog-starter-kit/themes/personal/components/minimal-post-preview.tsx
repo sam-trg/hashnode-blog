83d47bbf87753ad4f53a293f7961e783ebe5e55a
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-06-15 12:35:56 +0530

    Update minimal-post-preview.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -16,7 +16,7 @@ export const MinimalPostPreview = ({ title, date, slug, commentCount }: Props) =
 	const postURL = `/${slug}`;
 
 	return (
-		<section className="flex flex-col items-start gap-1 bg-white shadow-lg">
+		<section className="flex flex-col items-start gap-1 bg-zinc-100">
 			<h2 className="text-lg leading-tight tracking-tight text-black dark:text-white">
 				<Link href={postURL}>{title}</Link>
 			</h2>
