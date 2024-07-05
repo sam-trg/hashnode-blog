c113e1655d192066e89c311aa4a3d12c51cfd727
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-07-05 17:06:17 +0530

    Update personal-theme-header.tsx

--- packages/blog-starter-kit/themes/personal/components/personal-theme-header.tsx
+++ packages/blog-starter-kit/themes/personal/components/personal-theme-header.tsx
@@ -60,7 +60,7 @@ export const PersonalHeader = () => {
 	);
 
 	return (
-		<header className="grid grid-cols-2 items-center gap-5 bg-stone-100 ">
+		<header className="grid grid-cols-2 items-center gap-5 bg-stone-700 text-white ">
 			<div className="col-span-full md:col-span-1">
 				<h1>
 					<Link
