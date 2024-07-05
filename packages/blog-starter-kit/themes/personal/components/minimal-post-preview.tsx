b9499f54b62fbd58054a01dc6895108c9b69af90
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-07-05 17:09:41 +0530

    Update minimal-post-preview.tsx

--- packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
+++ packages/blog-starter-kit/themes/personal/components/minimal-post-preview.tsx
@@ -17,11 +17,11 @@ export const MinimalPostPreview = ({ title, date, slug, commentCount }: Props) =
 
 	return (
 		<section className="flex flex-col items-start gap-1">
-			<h2 className="text-lg leading-tight tracking-tight text-black dark:text-white">
+			<h2 className="text-lg leading-tight tracking-tight text-white dark:text-white">
 				<Link href={postURL}>{title}</Link>
 			</h2>
 			<p className="flex flex-row items-center gap-2">
-				<Link href={postURL} className="text-sm text-neutral-600 dark:text-neutral-400">
+				<Link href={postURL} className="text-sm text-white dark:text-neutral-400">
 					<DateFormatter dateString={date} />
 				</Link>
 				{commentCount > 2 && (
