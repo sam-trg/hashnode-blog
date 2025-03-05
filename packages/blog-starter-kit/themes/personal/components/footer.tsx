4511f275e0193973f5bf0574d66bc6a42473ef40
Samarth Asija <90459209+sam-trg@users.noreply.github.com>
2025-03-05 13:13:55 +0530

    Update footer.tsx

--- packages/blog-starter-kit/themes/personal/components/footer.tsx
+++ packages/blog-starter-kit/themes/personal/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
 
 	return (// footer on home page only. looks better non-mono.
 		<footer className="border-t bg-blue-300 pt-10 text-sm text-black dark:border-neutral-800 dark:text-neutral-400">
-			&copy; {new Date().getFullYear()} Sam's Notebook
+			&copy; {new Date().getFullYear()} {"Sam's Notebook"}
 		</footer>
 	);
 };
